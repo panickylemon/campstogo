52102f1f78961700afad602e7f0dad97c49f89ce
Alexey <zolotukhin89@gmail.com>
2016-09-09 23:31:35 +0300

    zzz

--- js/main.js
+++ js/main.js
@@ -2,4 +2,11 @@ jQuery(document).ready(function ($) {
 
     $('input, textarea').placeholder();
 
-})
+
+    $('.selectpicker').selectpicker({
+      template: {
+        caret: '<span class="glyphicon glyphicon-chevron-down"></span>'
+      }
+    });
+
+});
