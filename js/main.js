785cb1b47b06b62d592bfa976757af782daa9f19
Alexey <zolotukhin89@gmail.com>
2016-09-10 17:40:53 +0300

    11

--- js/main.js
+++ js/main.js
@@ -3,13 +3,7 @@ jQuery(document).ready(function ($) {
     $('input, textarea').placeholder();
 
 
-    $('.selectpicker').selectpicker({
-      template: {
-        //caret: '<span class="arrow-select"></span>'
-        //caret: '<span class="fa fa-caret-down fa-lg"></span>'
-        caret: '<span class="glyphicon glyphicon-triangle-bottom"></span>'
-      }
-    });
+    $('.selectpicker').selectpicker();
     $('.selectpicker').parent().children().removeAttr('title');
     $('.selectpicker').on('hidden.bs.select', function (e) {
         $(this).parent().children().removeAttr('title');
