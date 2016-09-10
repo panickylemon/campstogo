95d79062d8c7ca5ab4c2d65c851042a02d4f9f54
panickylemon <alenka__1992@mail.ru>
2016-09-10 19:32:38 +0300

    form

--- js/main.js
+++ js/main.js
@@ -2,10 +2,19 @@ jQuery(document).ready(function ($) {
 
     $('input, textarea').placeholder();
 
-
     $('.selectpicker').selectpicker();
     $('.selectpicker').parent().children().removeAttr('title');
     $('.selectpicker').on('hidden.bs.select', function (e) {
         $(this).parent().children().removeAttr('title');
     });
+
+
+
+
+
+    $(".fa-bars").click(function() {
+        $(".menu-header>ul").slideToggle("slow");
+    });
 });
+
+
