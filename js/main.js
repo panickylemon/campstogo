3f489d139552c4282d4513d492124a61eda0b527
panickylemon <alenka__1992@mail.ru>
2016-09-15 01:51:13 +0300

    card

--- js/main.js
+++ js/main.js
@@ -23,6 +23,29 @@ jQuery(document).ready(function ($) {
     $(".fa-bars").click(function() {
         $(".menu-header>ul").slideToggle("slow");
     });
+
+
+    $('[placeholder]').focus(function() {
+        var input = $(this);
+        if (input.val() == input.attr('placeholder')) {
+            if (this.originalType) {
+                this.type = this.originalType;
+                delete this.originalType;
+            }
+            input.val('');
+            input.removeClass('placeholder');
+        }
+    }).blur(function() {
+        var input = $(this);
+        if (input.val() == '') {
+            if (this.type == 'password') {
+                this.originalType = this.type;
+                this.type = 'text';
+            }
+            input.addClass('placeholder');
+            input.val(input.attr('placeholder'));
+        }
+    }).blur();
 });
 
 
